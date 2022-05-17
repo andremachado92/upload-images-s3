17b675292ff7773df655a7f9d55eb42c949d2228
André Machado <dreebassplayer@gmail.com>
2022-05-17 11:31:21 -0400

    first commit

---
+++ src/test/java/com/andremachado92/br/UploadImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremachado92.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
